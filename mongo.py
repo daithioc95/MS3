31d6b92407784606458d07ba24fe12c3696c2f83
daithioc95 <daithioc@outlook.com>
2021-04-02 10:28:47 +0000

    Added book category for all dictionary items

--- mongo.py
+++ mongo.py
@@ -22,6 +22,34 @@ coll2 = conn[DATABASE][COLLECTION_AUTHORS]
 documents_authors = coll1.find({},{ "_id": 0, "Author": 1})
 # documents = coll1
 
+# with open('data.txt', 'r') as reader, open('data_copy.txt', 'w') as writer:
+#     for listitem in reader:
+#         # writer.listitem.add({"Book":"None"})
+#         print(listitem)
+
+# with open('data.txt', 'r') as reader:
+#     dog_breeds = reader.readlines()
+# with open('data_copy.txt', 'w') as writer:
+#     for listitem in dog_breeds:
+#         writer.write(listitem.add({"Book":"None"}))
+
+# with open('quotes_test.json', 'w') as file:
+#     for listitem in file:
+#         listitem.add({"Book":"None"})
+#         print(listitem)
+
+# ***Adding categor to entire list
+with open('data.txt') as json_file:
+    data = json.load(json_file)
+    store_list = []
+    for p in data:
+        p['Book'] = ''
+        store_list.append(p)
+        # print(p)
+with open('data_copy.txt', 'w') as filehandle:
+    json.dump(store_list, filehandle)
+    # p.add({"Book":"None"})
+
 # create "book" key for all dictionary values
 # for doc in documents:
 #     doc.update_one({"Quote": coll1.find_one()},{$set:{"Book": 1}})
@@ -44,9 +72,11 @@ documents_authors = coll1.find({},{ "_id": 0, "Author": 1})
 
 # Upload quotes from data.txt to quotes collection
 
+# *****To insert data.txt content to collection
 # with open('data.txt') as json_file:
 #     data = json.load(json_file)
 #     for p in data:
 #         coll.insert_one(p)
 
+# Remove Dupes
 # coll.ensureIndex( { "Quote":1 }, { unique:true, dropDups:true } )
