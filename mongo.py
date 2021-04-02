6177e474051d684bb1e6a0668f7f1187303ef283
daithioc95 <daithioc@outlook.com>
2021-04-02 12:20:46 +0000

    Split Author and book name to separate stings in db

--- mongo.py
+++ mongo.py
@@ -22,10 +22,17 @@ coll2 = conn[DATABASE][COLLECTION_AUTHORS]
 documents_authors = coll1.find({},{ "_id": 0, "Author": 1})
 # documents = coll1
 
-# *** Delete all documents
-coll1.delete_many({})
 
-# ***Adding category to entire list
+# ****To split and update the Author and Books fields --tested
+for doc in documents_authors:
+    k = doc["Author"].split(",")
+    try:
+        coll1.update_one({"Author":doc["Author"]},{"$set": {"Book": k[1]}})
+        coll1.update_one({"Author":doc["Author"]},{"$set": {"Author": k[0]}})
+    except:
+        pass
+
+# ***Adding category to entire list --tested
 # with open('data.txt') as json_file:
 #     data = json.load(json_file)
 #     store_list = []
@@ -37,25 +44,18 @@ coll1.delete_many({})
 #     json.dump(store_list, filehandle)
 #     # p.add({"Book":"None"})
 
-# ****To split and update the Author and Books fields
-# for doc in documents_authors:
-#     k = doc["Author"].split(",")
-#     # print(k[0])
-#     # coll1.update({"Author":doc},{"$set": {"Author": k[0]}})
-#     try:
-#         coll1.replaceone({"Author": doc["Author"]}, {"Author": k[0], "Book": k[1]})
-#         # coll1.update({"Author":doc},{"$set": {"Book": k[1]}})
-#     except:
-#         coll1.replaceone({"Author": doc["Author"]}, {"Author": k[0], "Book": "None"})
-#     print(coll1.find())
 
 # Upload quotes from data.txt to quotes collection
 
-# *****To insert data.txt content to collection
-with open('data.txt') as json_file:
-    data = json.load(json_file)
-    for p in data:
-        coll1.insert_one(p)
+
+# *** Delete all documents --tested
+# coll1.delete_many({})
+
+# *****To insert data.txt content to collection --tested
+# with open('data.txt') as json_file:
+#     data = json.load(json_file)
+#     for p in data:
+#         coll1.insert_one(p)
 
 # Remove Dupes
 # coll.ensureIndex( { "Quote":1 }, { unique:true, dropDups:true } )
