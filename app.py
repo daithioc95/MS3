7e4a0f0973c7bcb5237d732e1428f661020a5348
daithioc95 <daithioc@outlook.com>
2021-04-19 12:56:12 +0000

    combine get_quotes() & get_all_quotes()

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ mongo = PyMongo(app)
 # Function to get quotes
 @app.route("/get_quotes")
 def get_quotes():
+    get_fav = request.args.get('get_fav')
     qotd = mongo.db.quotes.find_one()
     page = request.args.get('page', 1, type=int)
     limit = int(5)
@@ -51,9 +52,9 @@ def get_quotes():
                 except:
                     pass
             # if user has favourite quotes
-            if fav_quotes1:
+            if fav_quotes1 and get_fav != "No":
                 # update the quotes and pages with users favourites
-                fav_quotes = mongo.db.quotes.find({"_id": {"$in":  fav_quotes1}})
+                fav_quotes = mongo.db.quotes.find({"_id": {"$in":  fav_quotes1}}).skip(skips).limit(limit)
                 # update the quotes documents
                 quotes = fav_quotes
                 # Update pagitation for updated quotes
@@ -70,47 +71,10 @@ def get_quotes():
         limit=limit, 
         qotd=qotd,
         final_page=final_page,
-        fav_quotes2=fav_quotes2
+        fav_quotes2=fav_quotes2,
+        get_fav=get_fav
     )
 
-
-# function to return all quotes for logged in users
-@app.route("/get_all_quotes")
-def get_all_quotes():
-    qotd = mongo.db.quotes.find_one()
-    page = request.args.get('page', 1, type=int)
-    limit = int(5)
-    skips = limit * (page - 1)
-    final_page = (mongo.db.quotes.count_documents({}))/(limit-1)
-    pages = range(1, int(final_page + 2))
-    quotes = mongo.db.quotes.find().sort("Popularity", -1).skip(skips).limit(limit)
-    # feed through favoutite id's so only favourite stars are checked
-    try:
-        # if user logged in 
-        if session["user"]:
-            # get array of id's for users favourite quotes
-            users_fav_quotes = mongo.db.users.find_one({"username": session["user"]})["fav_quote_ids"]
-            fav_quotes2 = []
-            # Extract quote id's and append to list
-            for x in users_fav_quotes:
-                try:
-                    fav_quotes2.append(x)
-                # if not in object id format, pass
-                except:
-                    pass
-    # if session["user"] not recognised, user is logged out
-    except KeyError:
-        fav_quotes2 = []
-    return render_template(
-        'quotes.html', 
-        quotes=quotes,
-        page=page,
-        pages=pages,
-        limit=limit, 
-        qotd=qotd,
-        final_page=final_page,
-        fav_quotes2=fav_quotes2
-    )
 
 # https://www.youtube.com/watch?v=XYx5sIbU8B4
 # https://www.youtube.com/watch?v=v2TSTKlrPwo
@@ -139,7 +103,6 @@ def add_fav_quote():
 # Function to get authors
 def get_authors():
     get_fav = request.args.get('get_fav')
-    print(get_fav)
     page = request.args.get('page', 1, type=int)
     limit = int(5)
     skips = limit * (page - 1)
