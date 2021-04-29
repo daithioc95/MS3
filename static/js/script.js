53854e21dbc60c46403713b6175121be04134a6c
daithioc95 <daithioc@outlook.com>
2021-04-29 17:32:33 +0000

    Copy quote link to clipboard funcitonality

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,19 @@ $(document).ready(function () {
     
 });
 
+// https://stackoverflow.com/questions/63033012/copy-the-text-to-the-clipboard-without-using-any-input
+function shareQuote(quote_id) {
+  var TempText = document.createElement("input");
+  TempText.value = "http://ms3-quotes.herokuapp.com/share_quotes/" + quote_id;
+  document.body.appendChild(TempText);
+  TempText.select();
+  
+  document.execCommand("copy");
+  document.body.removeChild(TempText);
+  
+  alert("Copied sharable link: " + TempText.value);
+}
+
 // $(.mood-button).change(function(e) {
 //     if ($(this).is(':not(:checked)')) {
 //             $(this).addClass( "input[type=checkbox] + label" );}
