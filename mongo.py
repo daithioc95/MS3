0fd2d0d1680cb505235bec0511df87005e015131
daithioc95 <daithioc@outlook.com>
2021-05-03 16:22:59 +0000

    Add sort by date functionality to author and book comments

--- mongo.py
+++ mongo.py
@@ -27,9 +27,10 @@ documents = coll1.find()
 documents2 = coll2.find()
 documents3 = coll3.find()
 
-# main_authors = coll1.find().sort("Popularity", -1).limit(100)
+# main_authors = coll1.find().sort("Popularity", -1).limit(200)
 # for x in main_authors:
 #     print(x["Author"])
+
 # tags=[]
 # frequent = []
 # for doc in documents:
